a1f15b9fb5b8b3031ae41bd5e9eac72c819fd0ea
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-02-11 15:12:44 +0430

    change

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-const appContainer = document.getElementById("root");
-const root = createRoot(appContainer);
-root.render(<div>hello</div>);
