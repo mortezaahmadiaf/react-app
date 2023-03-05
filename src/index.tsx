81b8fed47fb5a6811c607a98ba08e9712ac1564c
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-03-05 19:25:14 +0430

    import scss file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import './styles/index.css'
+
 import { createRoot } from "react-dom/client";
+import * as React from 'react'
+import './styles/index.css'
+import './styles/index.scss'
 const appContainer = document.getElementById("root");
 const root = createRoot(appContainer);
-root.render(<div className='test'>hello</div>);
+root.render(<div className='test d-flex bg-dark text-white'>hello</div>);
