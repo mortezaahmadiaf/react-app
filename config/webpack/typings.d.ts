93d86b60d0f1c1ff85bd89a30c7a79c201627972
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-03-07 11:46:42 +0430

    change

--- config/webpack/typings.d.ts
+++ config/webpack/typings.d.ts
@@ -3,7 +3,7 @@ declare module '*.json' {
   export default value;
 }
 
-// declare module "*.jpg";
-// declare module "*.png";
-// declare module "*.jpeg";
-// declare module "*.gif";
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.gif";
