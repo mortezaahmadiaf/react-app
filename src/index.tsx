117671e78aa960659d73534a8741a2d4b30a8c59
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-03-09 18:28:21 +0430

    change

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import * as React from 'react'
 import { createRoot } from "react-dom/client";
 import './styles/index.css'
 import './styles/index.scss'
-import  image from './assets/image/morteza.jpg'
+import  image from 'assets/images/morteza.jpg'
 const appContainer = document.getElementById("root");
 const root = createRoot(appContainer);
-root.render(<div className='test d-flex bg-dark text-white'>
+root.render(<div className='test d-flex bg-dark text-white  gotham-Light-font'>
     <img
     
     src={image}
     />
+'d;lkjfg'dklfz lklkj'kl;;gdf ;lkdg;ldkf jhskldjgskldjkjsa kasjhdfklasj 
+
+    hello 
 </div>);
